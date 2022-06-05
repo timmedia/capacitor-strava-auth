e51c16e963fc044ca05f65b5afefb199f8f5f68a
timmedia <contact@tim-media.com>
2022-06-05 16:33:33 +0200

    refactor: ran formatter

--- android/src/main/java/com/timmedia/plugins/stravaauth/StravaAuthPlugin.java
+++ android/src/main/java/com/timmedia/plugins/stravaauth/StravaAuthPlugin.java
@@ -2,7 +2,6 @@ package com.timmedia.plugins.stravaauth;
 
 import android.content.Intent;
 import android.net.Uri;
-
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
@@ -16,15 +15,16 @@ public class StravaAuthPlugin extends Plugin {
 
     @PluginMethod
     public void authorize(PluginCall call) {
-         Uri intentUri = Uri.parse("https://www.strava.com/oauth/mobile/authorize")
-                 .buildUpon()
-                 .appendQueryParameter("client_id", call.getString("clientId"))
-                 .appendQueryParameter("redirect_uri", call.getString("redirectUri"))
-                 .appendQueryParameter("response_type", call.getString("responseType"))
-                 .appendQueryParameter("approval_prompt", call.getString("approvalPrompt"))
-                 .appendQueryParameter("scope", call.getString("scope"))
-                 .appendQueryParameter("state", call.getString("state"))
-                 .build();
+        Uri intentUri = Uri
+            .parse("https://www.strava.com/oauth/mobile/authorize")
+            .buildUpon()
+            .appendQueryParameter("client_id", call.getString("clientId"))
+            .appendQueryParameter("redirect_uri", call.getString("redirectUri"))
+            .appendQueryParameter("response_type", call.getString("responseType"))
+            .appendQueryParameter("approval_prompt", call.getString("approvalPrompt"))
+            .appendQueryParameter("scope", call.getString("scope"))
+            .appendQueryParameter("state", call.getString("state"))
+            .build();
 
         Intent intent = new Intent(Intent.ACTION_VIEW, intentUri);
         getContext().startActivity(intent);
--- ios/Plugin/StravaAuthPlugin.swift
+++ ios/Plugin/StravaAuthPlugin.swift
@@ -10,7 +10,7 @@ import AuthenticationServices
 public class StravaAuthPlugin: CAPPlugin {
     private let implementation = StravaAuth()
     private var authSession: ASWebAuthenticationSession?
-    
+
     @objc func authorize(_ call: CAPPluginCall) {
         let clientId = call.getString("clientId")?.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) ?? ""
         let redirectUri = call.getString("redirectUri")?.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) ?? ""
@@ -18,17 +18,16 @@ public class StravaAuthPlugin: CAPPlugin {
         let approvalPrompt = call.getString("approvalPrompt")?.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) ?? ""
         let scope = call.getString("scope")?.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) ?? ""
         let state = call.getString("state")?.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) ?? ""
-        
-        
+
         let appOAuthUrlStravaScheme = URL(string: "strava://oauth/mobile/authorize?client_id=\(clientId)&redirect_uri=\(redirectUri)&response_type=\(responseType)&approval_prompt=\(approvalPrompt)&scope=\(scope)&state=\(state)")!
 
         let webOAuthUrl = URL(string: "https://www.strava.com/oauth/mobile/authorize?client_id=\(clientId)&redirect_uri=\(redirectUri)&response_type=\(responseType)&approval_prompt=\(approvalPrompt)&scope=\(scope)&state=\(state)")!
-        
+
         if UIApplication.shared.canOpenURL(appOAuthUrlStravaScheme) {
             UIApplication.shared.open(appOAuthUrlStravaScheme, options: [:])
         } else {
             authSession = ASWebAuthenticationSession(url: webOAuthUrl, callbackURLScheme: "https://") {
-                url, error in
+                _, _ in
             }
         }
         call.resolve()
